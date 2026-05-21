d9b0d01a9423d37fb836cb1e882c7d368a835902
Frns-W <fwhennardy@gmail.com>
2026-05-21 12:05:49 +0700

    penyesuaian error

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Models\Prodi;
 use App\Models\Mahasiswa;
 use App\Models\Nilai;
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
