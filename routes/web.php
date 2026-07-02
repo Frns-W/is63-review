d211f7b7b3fc013601ab26e0593a3b4d854a4154
Frns-W <fwhennardy@gmail.com>
2026-07-02 11:53:55 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\NilaiController;
+use App\Http\Controllers\ProdiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::resource('prodi', ProdiController::class);
+Route::resource('mahasiswa', MahasiswaController::class);
+Route::resource('nilai', NilaiController::class);
 
 Route::get('/welcome', function () {
     return view('welcome');
