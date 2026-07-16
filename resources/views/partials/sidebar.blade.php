c74fc023a9e9b98853bada14cfebe363423080e7
Frns-W <fwhennardy@gmail.com>
2026-07-16 08:59:21 +0700

    partial

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -10,43 +10,39 @@
 
     <hr class="sidebar-divider my-0">
 
-    {{-- Menu: Dashboard
     <li class="nav-item {{ request()->routeIs('dashboard') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('dashboard') }}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span>
         </a>
-    </li> --}}
+    </li>
 
     <hr class="sidebar-divider">
     <div class="sidebar-heading">Data Master</div>
 
-    {{-- Menu: Program Studi
     <li class="nav-item {{ request()->routeIs('prodi.*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('prodi.index') }}">
             <i class="fas fa-fw fa-university"></i>
             <span>Program Studi</span>
         </a>
-    </li> --}}
+    </li>
 
-    {{-- Menu: Mahasiswa
     <li class="nav-item {{ request()->routeIs('mahasiswa.*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('mahasiswa.index') }}">
             <i class="fas fa-fw fa-users"></i>
             <span>Mahasiswa</span>
         </a>
-    </li> --}}
+    </li>
 
     <hr class="sidebar-divider">
     <div class="sidebar-heading">Akademik</div>
 
-    {{-- Menu: Nilai 
     <li class="nav-item {{ request()->routeIs('nilai.*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('nilai.index') }}">
             <i class="fas fa-fw fa-chart-bar"></i>
             <span>Nilai Mahasiswa</span>
         </a>
-    </li> --}}
+    </li>
 
     <hr class="sidebar-divider d-none d-md-block">
 
