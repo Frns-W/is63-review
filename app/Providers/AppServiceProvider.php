8c5565028f86fa2fd1019210355cbaad16500dd8
Frns-W <fwhennardy@gmail.com>
2026-07-16 10:47:26 +0700

    app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,16 @@
 <?php
-// app/Providers/AppServiceProvider.php
- 
+
 namespace App\Providers;
- 
+
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
-<<<<<<< HEAD
-use Illuminate\Pagination\Paginator;
 
-=======
- 
->>>>>>> 93f3c832ecf478fe90b79c99a5ff6e32cb71a03d
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-<<<<<<< HEAD
-=======
         // Gunakan template pagination Bootstrap 4
->>>>>>> 93f3c832ecf478fe90b79c99a5ff6e32cb71a03d
         Paginator::useBootstrapFour();
     }
 }
+
